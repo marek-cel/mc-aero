567240242e2cf06fb1e6aec02f87584251d5ef8c
Marek Cel <marek.cel@gmail.com>
2024-02-04 18:40:16 +0100

    Change org name to unify it across projects

--- src/defs.h
+++ src/defs.h
@@ -130,7 +130,7 @@
 
 #define APP_NAME   "mc-aero"
 #define APP_VER    "0.2"
-#define ORG_NAME   "Marek_Cel"
+#define ORG_NAME   "marekcel.pl"
 #define ORG_DOMAIN "marekcel.pl"
 
 ////////////////////////////////////////////////////////////////////////////////
